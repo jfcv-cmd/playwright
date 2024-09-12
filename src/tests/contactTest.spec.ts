44c3de6160f6cf78c07209bacf78a3ee9568f59c
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-09-12 00:27:12 -0500

    Add test to support data driven testing.

--- src/tests/contactTest.spec.ts
+++ src/tests/contactTest.spec.ts
@@ -2,6 +2,35 @@ import { test } from "@playwright/test";
 import logger from "../utils/LoggerUtil";
 import LoginPage from "../pages/LoginPage";
 import { decrypt } from "../utils/CryptojsUtil";
+import contacts from "../testdata/contacts.json";
+
+type User = {
+    firstName: string;
+    lastName: string;
+}
+
+for (const contact of contacts as User[]) {
+    test(`data driven [ DD ] contact creation for ${contact.firstName} ${contact.lastName}`, async ({ page }) => {
+        logger.info("test for contact creation started...");
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigateToLoginPage();
+        await loginPage.fillUsername(decrypt(process.env.userid as string));
+        await loginPage.fillPassword(decrypt(process.env.password as string));
+
+        const homePage = await loginPage.clickLoginButton();
+
+        await homePage.expectHomePageTitleToBeVisible();
+
+        const contactsPage = await homePage.navigateToContactsTab();
+
+        await contactsPage.createNewContact(contact.firstName, contact.lastName);
+        await contactsPage.expectContactLabelToContainName(contact.firstName, contact.lastName);
+
+        logger.info("test for contact creation completed.");
+    });
+}
 
 test('contact creation', async ({ page }) => {
     logger.info("test for contact creation started...");
@@ -25,4 +54,4 @@ test('contact creation', async ({ page }) => {
     await contactsPage.expectContactLabelToContainName(firstName, lastName);
 
     logger.info("test for contact creation completed.");
-})
+});
